78bca7b440469afd7740295dc03c5e8756b44172
Brendan Chico <boomaster40@gmail.com>
2022-10-13 15:57:55 -0400

    Card generation

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+const cardContainer = document.querySelector(".card-container");
+
+for (let i = 0; i < 16; i++) {
+  const newCard = document.createElement("div");
+  cardContainer.append(newCard);
+  newCard.classList.add("card");
+  // TO DO: Add randomization of cards/images
+  // newCard.setAttribute("data-id", i);
+}
